4c2ffc344cd92de3b8eae89e2acca7caf05c6145
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-10-22 05:13:11 +0000

    type annotated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing
     a start index and an end index corresponding to 
     the range of indexes to return in a list 
