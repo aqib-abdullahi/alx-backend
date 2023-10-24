899b1028f03c9c7c9519e848052da68fa36ae724
aqibabdullahi <abdullahiaqib@gmail.com>
2023-10-24 22:44:59 +0100

    MRUCache task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""MRUCache task 3
+"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache implementation inheriting from
+    BaseCaching
+    """
+    def __init__(self):
+        """Initializer
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """gets item associated with passed key
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
