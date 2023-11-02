e35423a4b03ff3089e5ea4876486b458e3c8c66f
aqibabdullahi <abdullahiaqib@gmail.com>
2023-11-02 08:03:03 +0100

    config babel task1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""flask app task1
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app, default_locale="en", default_timezone="UTC")
+
+
+class Config:
+    """class configures available
+    languages
+    """
+    LANGUAGES = ["en", "fr"]
+    babel_default_locale = "en"
+    babel_default_timezone = "UTC"
+
+
+@app.route('/')
+def home():
+    """Home page
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.config.from_object(Config)
+    app.run()
