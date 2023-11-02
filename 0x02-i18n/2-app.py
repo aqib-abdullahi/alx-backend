be574b88469123eae39fcce7918b86dadf131a4b
aqibabdullahi <abdullahiaqib@gmail.com>
2023-11-02 17:01:43 +0100

    determines best language fit

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,7 @@
 """flask app task1
 """
 from flask import Flask, render_template
-from flask_babel import Babel
+from flask_babel import Babel, request
 
 
 class Config:
@@ -19,11 +19,19 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """Determines best fit or match with our
+    supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def home():
     """Home page
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
